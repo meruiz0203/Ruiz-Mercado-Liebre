f487d0a09706e83408404faa72e9b636e518c384
meruiz <meruiz0203@gmail.com>
2023-08-21 21:40:24 -0300

    subiendo proyecto final

--- src/app.js
+++ src/app.js
@@ -5,11 +5,26 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`Se prendió en el puerto ${PORT}`);
+  console.log(`Se prendio en el puerto ${PORT}`);
 });
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/home.html"));
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.post("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
